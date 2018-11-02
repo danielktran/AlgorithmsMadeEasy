b7c8a295681e9257d2472a616d15455b863c1c87
danielktran <Dano@100.100.100.71>
2018-11-01 18:30:08 -0700

    Merge sort class

---
+++ src/algorithms_made_easy/MergeSort.java
@@ -0,0 +1,21 @@
+package algorithms_made_easy;
+
+/**
+ * Class for merge sort
+ * @author Dano
+ *
+ */
+
+public class MergeSort 
+{
+	public static int[] sort(int [] array)
+	{
+		mergeSort(0, array.length - 1, array);
+		return array;
+	}
+	
+	public static void mergeSort(int leftIndex, int rightIndex, int [] array)
+	{
+		
+	}
+}
